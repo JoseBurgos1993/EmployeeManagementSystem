c7852745131a69d313a3601abf2d6f83114f5aac
JoseBurgos1993 <burgosjd1993@gmail.com>
2020-08-17 16:40:18 -0500

    Updated inital mysql queries

--- Master/db/employee_db.sql
+++ Master/db/employee_db.sql
@@ -1,11 +1,26 @@
-drop employees if exists;
+drop database if exists employees;
 
 create database employees;
 
 use employees;
 
-create table employee{
-    id int auto increment primary key,
+create table department(
+    id int auto_increment primary key,
+    name varchar(30) not null
+);
+
+create table role(
+    id int auto_increment primary key,
+    title varchar(30) not null,
+    salary decimal not null,
+    department_id int not null,
+    constraint `fk_department_id`
+    foreign key (department_id)
+    references department(id) on update cascade on delete restrict
+);
+
+create table employee(
+    id int auto_increment primary key,
     first_name varchar(30) not null,
     last_name varchar(30) not null,
     role_id int not null,
@@ -16,16 +31,4 @@ create table employee{
     constraint `fk_manager_id`
     foreign key (manager_id)
     references employee(id) on update cascade on delete restrict
-};
-
-create table department{
-    id int primary key,
-    name varchar(30)
-};
-
-create table role{
-    id int primary key,
-    title varchar(30),
-    salary decimal,
-    department_id int foreign key
-};
+);
